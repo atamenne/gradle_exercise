fb4b72e524418e4befc5150a6b7f304287e4c187
Brian Ashworth <bosrsf04@gmail.com>
2017-01-30 10:11:46 -0500

    Updated build.gradle and exception handling in Java~

--- src/main/java/Numbers/Numbers.java
+++ src/main/java/Numbers/Numbers.java
@@ -1,38 +1,45 @@
 package Numbers;
 
-public class Numbers 
+public class Numbers
 {
 	public static void main(String[] args)
 	{
-	
-		if(args.length != 2)
+
+		if (args.length != 2)
 		{
 			System.out.println("Incorrect number of arguments!");
-			System.exit(1);
+            System.out.println("Usage: (lazy | triangle) <integer>");
+			System.exit(args.length + 1);
 		}
-		
+
 		String type = args[0];
-		int n = Integer.parseInt(args[1]);
-		
-		if(type.equals("triangle"))
+        int n = 0;
+        try {
+    		n = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid integer");
+            System.exit(2);
+        }
+
+		if (type.equals("triangle"))
 			System.out.println("Tri(" + n + ") = " + TriNum(n));
 		else if (type.equals("lazy"))
-			System.out.println("Lazy(" + n + ") = " + Caterer(n))
+			System.out.println("Lazy(" + n + ") = " + Caterer(n));
 		else
 		{
 			System.out.println("Error with command line args");
 			System.exit(1);
 		}
 	}
-	
+
 	public static int TriNum(int n)
 	{
 		if (n == 1)
 			return 1;
-			
+
 		return n + TriNum(n - 1);
-	}	
-	
+	}
+
 	public static int Caterer(int n)
 	{
 		int p = (n*n + n + 2) / 2;
