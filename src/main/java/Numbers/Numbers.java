c3f2927950e998a5d521afd2949249d138ceb86c
Brian Ashworth <bosrsf04@gmail.com>
2017-01-30 10:19:56 -0500

    Checked for positive integers. Removed space/tabs mixing

--- src/main/java/Numbers/Numbers.java
+++ src/main/java/Numbers/Numbers.java
@@ -2,47 +2,57 @@ package Numbers;
 
 public class Numbers
 {
-	public static void main(String[] args)
-	{
+    public static void main(String[] args)
+    {
 
-		if (args.length != 2)
-		{
-			System.out.println("Incorrect number of arguments!");
-            System.out.println("Usage: (lazy | triangle) <integer>");
-			System.exit(args.length + 1);
-		}
+        if (args.length != 2)
+        {
+            System.out.println("Incorrect number of arguments!");
+            System.out.println("Usage: (lazy | triangle) <postive-integer>");
+            System.exit(args.length + 1);
+        }
 
-		String type = args[0];
+        String type = args[0];
         int n = 0;
         try {
-    		n = Integer.parseInt(args[1]);
+            n = Integer.parseInt(args[1]);
         } catch (NumberFormatException e) {
             System.out.println("Invalid integer");
             System.exit(2);
         }
 
-		if (type.equals("triangle"))
-			System.out.println("Tri(" + n + ") = " + TriNum(n));
-		else if (type.equals("lazy"))
-			System.out.println("Lazy(" + n + ") = " + Caterer(n));
-		else
-		{
-			System.out.println("Error with command line args");
-			System.exit(1);
-		}
-	}
+        if (n < 0)
+        {
+            System.out.println("N is not positive");
+            System.exit(2);
+        }
 
-	public static int TriNum(int n)
-	{
-		if (n == 1)
-			return 1;
+        if (type.equals("triangle"))
+        {
+            System.out.println("Tri(" + n + ") = " + TriNum(n));
+        }
+        else if (type.equals("lazy"))
+        {
+            System.out.println("Lazy(" + n + ") = " + Caterer(n));
+        }
+        else
+        {
+            System.out.println("Error with command line args");
+            System.exit(1);
+        }
+    }
 
-		return n + TriNum(n - 1);
-	}
+    public static int TriNum(int n)
+    {
+        if (n == 1)
+            return 1;
 
-	public static int Caterer(int n)
-	{
-		int p = (n*n + n + 2) / 2;
-		return p;
-	}
+        return n + TriNum(n - 1);
+    }
+
+    public static int Caterer(int n)
+    {
+        int p = (n*n + n + 2) / 2;
+        return p;
+    }
 }
